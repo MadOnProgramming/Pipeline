1dc068589dc868937821cda3aa9d8e21390ce53c
MadOnProgramming <madhansekar12031990@gmail.com>
2025-04-17 10:27:13 +0530

    Create PipelineResponse.cs

---
+++ Core.Runtime.Pipelines/Models/PipelineResponse.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Runtime.Pipelines
+{
+    public class PipelineResponse
+    {
+        public string FlowInstanceId { get; set; }
+
+        public string Status { get; set; }
+    }
+}
